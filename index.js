ad9aa88a40d44a5ac1d9f92adb98c35f85cba03e
Raphaël Degraeve <raphael.degraeve@epitech.eu>
2018-03-01 20:10:18 +0100

    reading input file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('heheh');
+var fs          = require('fs');
+const _         = require("lodash");
+
+// Read file input
+const filename = process.argv[2];
+const filepath = ('./' + filename);
+var rows = fs.readFileSync(filepath).toString().split("\n");
+// Process file
+_.each(rows, function (row) {
+    console.log(row);
+});
