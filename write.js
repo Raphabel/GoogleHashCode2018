d0afc0ab105666ff4a34f9e89fef471457807da0
Raphaël Degraeve <raphael.degraeve@epitech.eu>
2018-03-01 21:29:01 +0100

    fonction write file

---
+++ write.js
@@ -0,0 +1,25 @@
+const _         = require("lodash");
+var fs          = require('fs');
+
+var Write = { };
+
+// Write file input
+Write.displaySolution = function (filename, cars) {
+    var path = ("./outputs/solution-" + filename).replace(/.in/i, ".txt");
+    
+    var stream = fs.createWriteStream(path);
+    stream.once('open', function (fd) {
+        _.each(cars, function (car) {
+            _.each(car.rides, function (ride, index) {
+                stream.write(ride.toString());
+                if (index <  (car.rides.length - 1)) {
+                    stream.write(" ");
+                }
+            });
+            stream.write("\n");
+        });
+        stream.end();
+    });
+}
+
+module.exports = Write;
